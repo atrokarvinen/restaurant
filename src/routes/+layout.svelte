da9b8f7110937b434f5ec755233f9e5ffecda965
Atro Karvinen <atrok@hotmail.com>
2023-09-26 21:20:29 +0300

    added mobile anchor links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,11 +11,12 @@
 	import '../app.postcss';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+	import { page } from '$app/stores';
 	import Button from '$lib/components/Button.svelte';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import Header from './Header.svelte';
 	import { cartStore, defaultCart } from './cart/cartStore';
-	import { authStore } from './login/authStore';
+	import { authStore, isAdmin } from './login/authStore';
 
 	export let data;
 
@@ -27,16 +28,39 @@
 	$: console.log('[UI] loaded cart:', data.cart);
 
 	const drawerStore = getDrawerStore();
+
+	$: isAdminVisible = isAdmin($authStore);
+	$: {
+		console.log('path:', $page.url.pathname);
+		drawerStore.close();
+	}
+	$: path = $page.url.pathname;
 </script>
 
 <Modal />
 <Toast position="t" />
 <Drawer>
 	{#if $drawerStore.id === 'mobile-nav'}
-		<div>
-			<span>Meta:</span>
-			<span>{$drawerStore.meta.test}</span>
-
+		<div class="p-4">
+			<nav class="list-nav pb-5">
+				<ul>
+					<li>
+						<a href="/" class={path === '/' ? 'bg-primary-active-token' : ''}>Home</a>
+					</li>
+					<li>
+						{#if isAdminVisible}
+							<a href="/admin" class={path.includes('/admin') ? 'bg-primary-active-token' : ''}
+								>Admin</a
+							>
+						{/if}
+					</li>
+					<li>
+						<a href="/login" class={path.includes('/login') ? 'bg-primary-active-token' : ''}
+							>Login</a
+						>
+					</li>
+				</ul>
+			</nav>
 			<Button on:click={() => drawerStore.close()}>Close</Button>
 		</div>
 	{:else}
